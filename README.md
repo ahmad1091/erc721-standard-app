817d0a8ea7e23afcbf1bf1ee7fd8c66dd899c82b
ahmad1091 <ahmadtayeb91@gmail.com>
2021-06-10 19:10:05 +0300

    build the frontend and add description for the readme.md

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # erc721-standard-app
 Crypto Collectible: ERC-721 NFT App
+
+## Abstract:
+
+NFT(Non-fungible token).
+
+“Non-fungible” more or less means that it’s unique and can’t be replaced with something else. For example, a bitcoin is fungible — trade one for another bitcoin, and you’ll have exactly the same thing. A one-of-a-kind trading card, however, is non-fungible. If you traded it for a different card, you’d have something completely different.
+
+At a very high level, most NFTs are part of the Ethereum blockchain. Ethereum is a cryptocurrency, like bitcoin or dogecoin, but its blockchain also supports these NFTs, which store extra information that makes them work differently from, say, an ETH coin. It is worth noting that other blockchains can implement their own versions of NFTs.
+
+NFTs can really be anything digital (such as drawings, music, your brain downloaded and turned into an AI), but a lot of the current excitement is around using the tech to sell digital art.
+### NFT based on ERC721 standard, What does that mean and, how it differs from ERC20 standard?
+* **[ERC-20](https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md)**: 
+ERC-20 specifies a common list of rules for all Ethereum tokens, which means that this token allows developers of all types to accurately predict how the new codes work within the larger Ethereum system. The ERC-20 identifies six different functions for the benefit of other codes in the Ethereum system. These are general problems with basic functions, which include how to move tokens, how tokens function and how users can access data about a token. The Ethereum community created these standards with three optional rules, and six mandatories.
+
+* **[ERC-721](https://github.com/ethereum/EIPs/blob/master/EIPS/eip-721.md)**: 
+This token allows the ownership of unique digital assets. Its biggest implication is Cryptokitties. ERC721, is an Ethereum Improvement Proposal (EIP), which is a proposed standard that would allow smart contracts to operate as trade-able tokens similar to ERC20. ERC721 tokens are unique in that the tokens are non-fungible. ERC721 tokens can be used in any exchange, but their value is a result of the uniqueness and rareness associated with each token.
+
+**[ERC-20](https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md)**: For money and money-like tokens.
+
+**[ERC-721](https://github.com/ethereum/EIPs/blob/master/EIPS/eip-721.md)**: For things and thing-like tokens.
+
+
+## Features:
+
+* User can display minted colors for the same token.
+* User can mint NFT colors for the same token address.
+
+## this work built to work in the loaclnetwork:localhost:8545
+
+## Link to the app: [erc721-standard-app]()
--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,125 @@
 import React, { Component } from 'react';
 import './App.css';
+import Web3 from 'web3'
+import Color from '../abis/Color.json';
 
 class App extends Component {
+  
+  constructor(props){
+    super(props);
+    this.state ={
+      account:'',
+      colors:[],
+      contract:null,
+    };
+  }
+
+  async componentWillMount(){
+    await this.loadWeb3()
+    await this.loadBlockchainData();
+  }
+  async loadBlockchainData(){
+    const web3 = window.web3;
+    const accounts = await web3.eth.getAccounts();
+    this.setState({account:accounts[0]});
+    const networkId = await web3.eth.getId();
+    if (Color.networks[networkId]) {
+      const address = Color.networks[networkId].address;//   Color.networks[window.ethereum.networkVersion]
+      const abi = Color.abi
+      const colorsContract = new web3.eth.Contract(
+       abi,
+        address,
+      );
+      this.setState({contract:colorsContract});
+      for (let index = 0; index < 6; index++) {
+          const color =  await colorsContract.methods.colors(index).call();
+          this.setState({
+            colors:[...this.state.colors,color]
+          })
+        }
+
+    }else{
+      alert('Smart Contract mot deployed to current network')
+    }  
+}
+
+  async loadWeb3(){
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    }
+    else if (window.web3) {
+  window.web3 =new Web3(window.web3.currentProvider)      
+    }
+    else{
+      window.alert('No ethereum avalable in the browser');
+    }
+  }
+
+   mint=(color)=>{
+    this.state.contract.methods.mint(color).send({from:this.state.account})
+    .once('receipt',(receipt)=>{
+          this.setState({
+            color:[...this.state.colors,color]
+          })
+    });
+  }
+
   render() {
     return (
-      <div>
-
+<div>
+        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+          <a
+            className="navbar-brand col-sm-3 col-md-2 mr-0"
+            href="#"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Color Tokens 
+          </a>
+          <ul className="navbar-nav px-3">
+            <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
+              <small className="text-white"><span id="account">{this.state.account}</span></small>
+            </li>
+          </ul>
+        </nav>
+        <div className="container-fluid mt-5">
+          <div className="row">
+            <main role="main" className="col-lg-12 d-flex text-center">
+              <div className="content mr-auto ml-auto">
+                <h1>Issue Token</h1>
+                <form onSubmit={(event) => {
+                  event.preventDefault()
+                  const color = this.color.value
+                  this.mint(color)
+                }}>
+                  <input
+                    type='text'
+                    className='form-control mb-1'
+                    placeholder='e.g. #FFFFFF'
+                    ref={(input) => { this.color = input }}
+                  />
+                  <input
+                    type='submit'
+                    className='btn btn-block btn-primary'
+                    value='MINT'
+                  />
+                </form>
+              </div>
+            </main>
+          </div>
+          <hr/>
+          <div className="row text-center">
+            { this.state.colors.map((color, key) => {
+              return(
+                <div key={key} className="col-md-3 mb-3">
+                  <div className="token" style={{ backgroundColor: color }}></div>
+                  <div>{color}</div>
+                </div>
+              )
+            })}
+          </div>
+        </div>
       </div>
     );
   }
