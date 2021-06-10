cb869026974abc14078a1d625ee682430db07cc8
ahmad1091 <ahmadtayeb91@gmail.com>
2021-06-10 14:34:01 +0300

    add the contracts and create tests

---
+++ src/contracts/Color.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+import './ERC721.sol';
+// import '@openzeppelin/contracts/token/ERC721/ERC721.sol'
+
+contract Color is ERC721{
+
+    constructor() ERC721("Color","COLOR"){}
+
+}
+
---
+++ test/color.test.js
@@ -0,0 +1,14 @@
+const Color = artifacts.require('./Color.sol');
+
+require('chai')
+.use(require('chai-as-promised'))
+.should()
+
+contract('Color',(accounts)=>{
+
+    describe('development',async()=>{
+        it('deploys Successfully',async()=>{
+            
+        })
+    })
+})
