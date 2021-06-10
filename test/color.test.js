b504d905b1740b0d8e303196ed68f4d934ce8317
ahmad1091 <ahmadtayeb91@gmail.com>
2021-06-10 14:57:41 +0300

    add the test files

--- test/color.test.js
+++ test/color.test.js
@@ -1,3 +1,5 @@
+const { assert } = require('chai');
+
 const Color = artifacts.require('./Color.sol');
 
 require('chai')
@@ -5,10 +7,28 @@ require('chai')
 .should()
 
 contract('Color',(accounts)=>{
-
+    let colorContract;
+    before(async()=>{
+        colorContract = await Color.deployed();
+    })
     describe('development',async()=>{
         it('deploys Successfully',async()=>{
-            
+             colorContract = await Color.deployed();
+             const address = colorContract.address;
+             assert.notEqual(address,0x0);
+             assert.notEqual(address,'');
+             assert.notEqual(address,null);
+             assert.notEqual(address,undefined);
+        })
+        it('has a name',async ()=>{
+            const name = await colorContract.name();
+            assert.equal(name,'Color');
+
+        })
+        it('has a Symbol',async ()=>{
+            const symbol = await colorContract.symbol();
+            assert.equal(symbol,'COLOR');
+
         })
     })
 })
