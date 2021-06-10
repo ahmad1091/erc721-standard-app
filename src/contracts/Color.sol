0d4c575be4ec9b5c7bd686d1d26dcc033a4e6134
ahmad1091 <ahmadtayeb91@gmail.com>
2021-06-10 17:05:29 +0300

    add more tests and modify App component

--- src/contracts/Color.sol
+++ src/contracts/Color.sol
@@ -5,8 +5,25 @@ import './ERC721.sol';
 // import '@openzeppelin/contracts/token/ERC721/ERC721.sol'
 
 contract Color is ERC721{
+    string[] public colors;
+    mapping(string => bool) _colorExists;
 
     constructor() ERC721("Color","COLOR"){}
+
+    
+    function mint(string memory _color) public{
+        //require color to be uniq
+        require(!_colorExists[_color]);
+        //add  the color
+        colors.push(_color);
+        uint _id = colors.length - 1;
+        //call the mint function
+        _mint(msg.sender,_id);
+        _colorExists[_color] = true;
+        //thrack the color
+
+
+    }
 
 }
 
--- test/color.test.js
+++ test/color.test.js
@@ -11,6 +11,7 @@ contract('Color',(accounts)=>{
     before(async()=>{
         colorContract = await Color.deployed();
     })
+
     describe('development',async()=>{
         it('deploys Successfully',async()=>{
              colorContract = await Color.deployed();
@@ -31,4 +32,37 @@ contract('Color',(accounts)=>{
 
         })
     })
+
+    describe('minting',async()=>{
+        it('create a new token',async()=>{
+            const result = await colorContract.mint('#EC058E');
+            //Success
+            const event = result.logs[0].args;
+            assert.equal(event.tokenId.toNumber(),0,'id is correct');
+            assert.equal(event.from,'0x0000000000000000000000000000000000000000','form is correct')
+            assert.equal(event.to,accounts[0],'to is correct');
+            
+            //Failure:cannt mint the same color more than one time
+            await colorContract.mint('#EC058E').should.be.rejected;
+        })
+    })
+
+    describe('indexing',async()=>{
+        it('Lists colors',async()=>{
+            //mint 3 more tokens
+            await colorContract.mint('#483d8b');
+            await colorContract.mint('#a52a2a');
+            await colorContract.mint('#008b8b');
+            const colors = ['#EC058E','#483d8b','#a52a2a','#008b8b'];
+            let temp = [];
+            for (let index = 0; index < 4; index++) {
+                const color = await colorContract.colors(index);
+                temp.push(color);
+            }
+            assert.equal(colors.join(','),temp.join(','));
+            }
+        )
+    })
+
+
 })
